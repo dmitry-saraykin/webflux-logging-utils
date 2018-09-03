364918e6f8c8f8cf34ca1082a282f5b243b708c1
Dmitry Saraykin <1506231@gmail.com>
2018-09-03 10:07:34 +0300

    AW-310

--- src/main/java/ru/roborox/logging/exception/LoggingExceptionHandler.java
+++ src/main/java/ru/roborox/logging/exception/LoggingExceptionHandler.java
@@ -4,11 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebExceptionHandler;
-
-import java.nio.file.AccessDeniedException;
 
 import reactor.core.publisher.Mono;
 import ru.roborox.logging.utils.LoggingUtils;
@@ -34,6 +33,7 @@ public class LoggingExceptionHandler implements WebExceptionHandler {
             } else if (ex instanceof AccessDeniedException) {
                 if (exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN)) {
                     logger.warn(marker, buildMessage(exchange.getRequest(), ex));
+                    return exchange.getResponse().setComplete();
                 }
             } else {
                 if (exchange.getResponse().setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR)) {
